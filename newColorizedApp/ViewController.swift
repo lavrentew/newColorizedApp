2db792ceb961efb256c7c998a8820337030d03a5
lavrentew <n89258101522@gmail.com>
2024-04-26 14:26:53 +0300

    Initial Commit

---
+++ newColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newColorizedApp
+//
+//  Created by Николай Лаврентьев on 4/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
